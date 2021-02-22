fa352cf23a2d25ab2d3205b0c6dc3f4f8fcb15af
Maciej Figlarz <maciejfiglarz333@gmail.com>
2021-02-22 21:06:28 +0100

    install storybooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from './views/Root/Root';
 import reportWebVitals from './reportWebVitals';
+// import Button from './Components/Button';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/views/Root/Root.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import Button from '../../Components/Button';
+
+const MyButton = styled.button`
+  background: red;
+`;
+const Root = () => <MyButton>test<Button>ddd</Button></MyButton>;
+
+export default Root;
